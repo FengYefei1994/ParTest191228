ccf8a7cadeb7c47917327fc786aaec26f989db6b
FYF <zxfyf361@163.com>
2019-12-28 16:58:52 +0800

    案例1修改了一个scan.close();功能

--- src/com/qianfeng/basicSyntax/Anli.java
+++ src/com/qianfeng/basicSyntax/Anli.java
@@ -6,6 +6,7 @@ public class Anli {
 		Scanner scan= new Scanner(System.in);  //扫描器
 		System.out.println("请输入球形半径：");
 		double r=scan.nextDouble();                  //接收数据
+		scan.close();
 		final double PI=3.1415926;                     //定义常量PI      定义常量语句：final  数据类型  常量名称[=值]         
 		double volume=4.0/3.0*PI*r*r*r;           //求球形的体积
 		System.out.println("球形体积是："+volume);
